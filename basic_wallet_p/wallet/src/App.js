52bd2412e275bab27d27f73b9564452c84abed7b
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-02-13 16:59:38 -0600

    completed MVP

--- basic_wallet_p/wallet/src/App.js
+++ basic_wallet_p/wallet/src/App.js
@@ -1,17 +1,91 @@
-import React, {useState, useEffect} from 'react';
-import Wallet from './components/Wallet'
+import React, { Component } from 'react';
+import Display from './components/Display';
+import Form from './components/Form';
+import axios from "axios";
 
-const baseURL = 'http://localhost:5000';
+class App extends Component {
+  state={
+    chain: [],
+    title: "Whole Chain",
+    coins: null,
+    filteredChain: [],
+    id: null
+  }
 
-function App() {
-  
+  componentDidMount() {
+    this.getChain()
+  }
 
-  return (
-    <div className="App">
-     
-    <Wallet />
-    </div>
-  );
+  getChain = () => {
+    axios.get('http://localhost:5000/chain')
+    .then(({data}) => this.setState({chain: data.chain}))
+    .catch(err => console.error(err))
+  }
+
+  filterChain = (id) => {
+    const filteredChain = this.state.chain.filter(block => {
+      if (!block.transactions.length) {
+        return false
+      }
+      else {
+        return block.transactions[0].recipient === id
+      } 
+    })
+    if (!filteredChain.length) {
+      this.setState({
+        warning: "No user by that id was found",
+        title: "Whole Chain",
+        filteredChain: [],
+        coins: null
+      })
+      setTimeout(() => {
+        this.setState({warning: ''})
+      }, 3000);
+    }
+    else {
+      this.setState(
+      {
+        id,
+        title: `${id}'s Transactions`,
+        filteredChain,
+        warning: null
+      }
+      )
+      setTimeout(() => {
+        this.getCoins(id)
+      }, 500);
+    }
+  } 
+
+  getCoins = (id) => {
+    let totalCoins = 0;
+    this.state.filteredChain.forEach(block => {
+      if(block.transactions[0].recipient === id) {
+        return totalCoins += block.transactions[0].amount
+      }
+      if(block.transactions[0].sender === id) {
+        return totalCoins -= block.transactions[0].amount
+      }
+    })
+    this.setState({coins: totalCoins})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form 
+          filterChain={this.filterChain}
+        />
+        <h2>{this.state.warning}</h2>
+        <Display 
+          coins={this.state.coins}
+          chain={this.state.filteredChain.length ? this.state.filteredChain : this.state.chain}
+          title={this.state.title}
+          length={this.state.chain.length}
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
